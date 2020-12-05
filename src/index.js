ec72bab3bb60f6fbd691792a40a4543a594ee2e3
Stas Chapanskiyi <stkr.stkr@gmail.com>
2020-12-05 22:53:48 +0200

    change new bug

--- src/index.js
+++ src/index.js
@@ -7,27 +7,32 @@ const body = document.body;
 const menu = document.querySelector('ul[class="menu js-menu"]');
 const themeSwitchToggle = document.getElementById('theme-switch-toggle');
 const markUp = itemsTemplate(recipies).trim();
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
 
 menu.insertAdjacentHTML('beforeend', markUp);
 
 if (localStorage.getItem('key')) {
   body.classList.add(localStorage.getItem('key'));
-  themeSwitchToggle.checked = true;
+  localStorage.getItem('key') === Theme.DARK
+    ? (themeSwitchToggle.checked = true)
+    : (themeSwitchToggle.checked = false);
 }
 
-themeSwitchToggle.addEventListener('change', () => {
-  ChangeTheme();
-});
+themeSwitchToggle.addEventListener('change', ChangeTheme);
 
 function ChangeTheme() {
   console.log(themeSwitchToggle.hasAttribute('checked'));
   if (!themeSwitchToggle.checked) {
-    body.classList.remove('dark-theme');
-    body.classList.add('light-theme');
-    localStorage.setItem('key', 'light-theme');
+    setTheme(Theme.DARK, Theme.LIGHT);
   } else {
-    body.classList.remove('light-theme');
-    body.classList.add('dark-theme');
-    localStorage.setItem('key', 'dark-theme');
+    setTheme(Theme.LIGHT, Theme.DARK);
   }
+}
+function setTheme(oldTheme, newTheme) {
+  body.classList.remove(oldTheme);
+  body.classList.add(newTheme);
+  localStorage.setItem('key', newTheme);
 }
